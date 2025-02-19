c223371754a945472bc26f7698aacf5e11e92783
DzmitryiKorjik <dmardovitch@gmail.com>
2025-02-19 10:38:37 +0100

    finir

--- app.py
+++ app.py
@@ -7,13 +7,16 @@
 
 import sys
 import PySide6
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QListWidget, QMessageBox
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QListWidget, QMessageBox, QListWidgetItem
+from PySide6 import QtCore
+from movie import get_movies
+from movie import Movie
 
 class MovieApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Movie List App")
+        self.setWindowTitle("Movie List")
         self.setGeometry(100, 100, 400, 300)
 
         # Champ d'entrée du film
@@ -39,23 +42,38 @@ class MovieApp(QWidget):
         layout.addWidget(self.remove_button)
 
         self.setLayout(layout)
+        self.load_movies()
+
+    def load_movies(self):
+        """Charge les films d'un fichier JSON dans la liste au démarrage"""
+        movies = get_movies()
+        for movie in movies:
+            lw_item = QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.movie_list.addItem(lw_item)
 
     def add_movie(self):
         """Ajoute un film à la liste"""
-        movie_name = self.movie_input.text().strip()
-        if movie_name:
-            self.movie_list.addItem(movie_name)
-            self.movie_input.clear()
+        movie_title = self.movie_input.text()
+        if movie_title:
+            movie = Movie(movie_title)
+            if movie.add_to_movies():
+                lw_item = QListWidgetItem(movie.title)
+                lw_item.setData(QtCore.Qt.UserRole, movie)
+                self.movie_list.addItem(lw_item)
+                self.movie_input.setText("")
         else:
-            QMessageBox.warning(self, "Erreur", "Saisir le titre du film!")
+            QMessageBox.warning(self, "Erreur", "Veuillez saisir un titre de film!")
 
     def remove_movie(self):
         """Supprime le film sélectionné"""
         selected_item = self.movie_list.currentItem()
         if selected_item:
-            self.movie_list.takeItem(self.movie_list.row(selected_item))
+            movie = selected_item.data(QtCore.Qt.UserRole)
+            if movie.remove_from_movies():
+                self.movie_list.takeItem(self.movie_list.row(selected_item))
         else:
-            QMessageBox.warning(self, "Erreur", "Sélectionner un film à supprimer!")
+            QMessageBox.warning(self, "Erreur", "Veuillez sélectionner un film!")
 
 # Lancement de l'application
 if __name__ == "__main__":
